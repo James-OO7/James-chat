5e76315e3b0263791fe55a959b4e73fc86bd0e46
James-OO7 <jamesbond03@icloud.com>
2024-09-20 13:04:03 -0400

    Server used to host James-chat front-end

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = 3001;
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for 'chatMessage' and broadcast the user + message
+  socket.on('chatMessage', (data) => {
+    console.log(`${data.user}: ${data.text}`); // Log with username for debugging
+    io.emit('chatMessage', { user: data.user, text: data.text }); // Broadcast message with username
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
